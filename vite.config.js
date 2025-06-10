a7b259ee188efd3f8068242f6f67a0120d1f6f23
ChiJiun <0311gino@gmail.com>
2025-06-11 00:57:31 +0800

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-vote-app/", // 替換為你的 GitHub 倉庫名稱
+  base: "/BlockchainVotingSystem/", // 替換為你的 GitHub 倉庫名稱
   build: {
     outDir: "dist",
     assetsDir: "assets",
