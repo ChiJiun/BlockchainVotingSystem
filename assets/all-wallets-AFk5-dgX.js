80f74f174bed8784944b93efcb9ff85276776a94
ChiJiun <0311gino@gmail.com>
2025-06-11 14:45:20 +0800

    Updates

---
+++ assets/all-wallets-AFk5-dgX.js
@@ -0,0 +1,6 @@
+import{b as o}from"./core-DW_3pEDJ.js";import"./index-D8QufNDE.js";import"./events-BkvgUXNg.js";import"./index.es-M3FGDyip.js";const r=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{r as allWalletsSvg};
