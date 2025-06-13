2c1feb3111f532495d149b6e238293472edf749e
ChiJiun <0311gino@gmail.com>
2025-06-13 23:01:32 +0800

    Updates

--- assets/cursor-DaV33rgV.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-L2eb5vhu.js";import"./index-DjYIxbWQ.js";import"./events-Dzv4wiHR.js";import"./index.es-B23QU08C.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
