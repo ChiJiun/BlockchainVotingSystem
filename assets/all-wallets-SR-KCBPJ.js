9c4be22b5b9ea997f0301f3ccf33b9798087f1d0
ChiJiun <0311gino@gmail.com>
2025-06-13 16:34:27 +0800

    Updates

--- assets/all-wallets-SR-KCBPJ.js
+++
@@ -1,6 +0,0 @@
-import{b as o}from"./core-B5p0GNO4.js";import"./index-CzSPx7k9.js";import"./events-C-33hTcA.js";import"./index.es-zXYJpIKl.js";const r=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{r as allWalletsSvg};
--- assets/cursor-Vecs-vBl.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-B5p0GNO4.js";import"./index-CzSPx7k9.js";import"./events-C-33hTcA.js";import"./index.es-zXYJpIKl.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
