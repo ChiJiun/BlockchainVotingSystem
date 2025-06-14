aaa4098190ee2160a534d8d8bea379ab39ec8e4f
ChiJiun <0311gino@gmail.com>
2025-06-14 17:03:57 +0800

    Updates

---
+++ assets/all-wallets-fFQ3WBMo.js
@@ -0,0 +1,6 @@
+import{b as o}from"./core-DCdaLJRE.js";import"./index-BJdmAQq1.js";import"./events-BGf-rX06.js";import"./index.es-DQgpAb_d.js";const r=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{r as allWalletsSvg};
